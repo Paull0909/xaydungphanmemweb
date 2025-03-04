ff62627c97634eab0cba29ef5ecc51ec532bc453
CongNhin <nguyenthanhcongnhin@gmail.com>
2025-03-05 01:47:47 +0700

    FE

--- Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d3cc2b5727b4d865b124a4667ab981fc7d30046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf97b67462f917e5c15ef618a579f3fab5892e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
