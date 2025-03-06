02b4e5196abc8d9d263ca5964e1052304ebab21e
CongNhin <nguyenthanhcongnhin@gmail.com>
2025-03-06 15:00:55 +0700

    Product
    6/3

--- Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3527cc5df3aabd7d4d048bbee3c80abf82c79c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87029805268fff5206ce2ceb1f1baf98dc1f0c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
