fc242e4954e9d6de3113fc0e21c26964d0096904
trongdat <letrongdat1142@gmail.com>
2025-03-06 17:28:48 +0700

    làm lại
    dddmm

--- Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87029805268fff5206ce2ceb1f1baf98dc1f0c5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b4e5196abc8d9d263ca5964e1052304ebab21e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
