1bbcd2ed03ae64010edf9bc1d0638cd8dd2a2dfd
trongdat <letrongdat1142@gmail.com>
2025-03-06 16:32:33 +0700

    cmt
    cnt

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3527cc5df3aabd7d4d048bbee3c80abf82c79c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87029805268fff5206ce2ceb1f1baf98dc1f0c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
